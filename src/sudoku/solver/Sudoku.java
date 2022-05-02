d8fc09e8f479568a3f438dd171457524f07951fe
Ilija Pejic <ipejic@student.ethz.ch>
2022-05-02 20:13:40 +0200

    all methods return now either arraylist or 2dArrays

--- src/sudoku/solver/Sudoku.java
+++ src/sudoku/solver/Sudoku.java
@@ -15,25 +15,25 @@ public class Sudoku {
 	public static void main(String[] args) {
 		
 		
-		int[][] solMat =  { { 4, 1, 0, 0, 6, 0, 0, 7, 0 }, 
-							{ 0, 0, 3, 0, 8, 5, 0, 0, 9 }, 
-							{ 0, 2, 0, 3, 7, 0, 5, 0, 1 },
-							{ 0, 3, 0, 6, 0, 9, 2, 5, 0 }, 
-							{ 6, 0, 0, 5, 0, 1, 0, 0, 0 }, 
-							{ 0, 0, 9, 0, 2, 0, 0, 0, 3 },
-							{ 0, 0, 6, 2, 0, 0, 7, 4, 5 }, 
-							{ 0, 0, 0, 4, 0, 6, 8, 0, 0 }, 
-							{ 2, 8, 4, 0, 0, 0, 1, 9, 6 } };
+//		int[][] solMat =  { { 4, 1, 0, 0, 6, 0, 0, 7, 0 }, 
+//							{ 0, 0, 3, 0, 8, 5, 0, 0, 9 }, 
+//							{ 0, 2, 0, 3, 7, 0, 5, 0, 1 },
+//							{ 0, 3, 0, 6, 0, 9, 2, 5, 0 }, 
+//							{ 6, 0, 0, 5, 0, 1, 0, 0, 0 }, 
+//							{ 0, 0, 9, 0, 2, 0, 0, 0, 3 },
+//							{ 0, 0, 6, 2, 0, 0, 7, 4, 5 }, 
+//							{ 0, 0, 0, 4, 0, 6, 8, 0, 0 }, 
+//							{ 2, 8, 4, 0, 0, 0, 1, 9, 6 } };
 		
-//		int[][] solMat =  {{0,7,0,0,0,5,0,0,0},
-//							{5,0,0,7,0,2,0,0,0},
-//							{0,3,0,0,8,0,0,7,0},
-//							{0,9,0,0,6,0,7,5,8},
-//							{0,0,0,0,3,0,0,0,0},
-//							{0,0,5,0,0,0,4,0,6},
-//							{0,0,9,0,0,0,6,0,5},
-//							{0,0,0,0,0,8,0,2,1},
-//							{8,0,0,0,9,0,0,0,0}	};
+		int[][] solMat =  {{0,7,0,0,0,5,0,0,0},
+							{5,0,0,7,0,2,0,0,0},
+							{0,3,0,0,8,0,0,7,0},
+							{0,9,0,0,6,0,7,5,8},
+							{0,0,0,0,3,0,0,0,0},
+							{0,0,5,0,0,0,4,0,6},
+							{0,0,9,0,0,0,6,0,5},
+							{0,0,0,0,0,8,0,2,1},
+							{8,0,0,0,9,0,0,0,0}	};
 		
 //		int[][] solMat = {{1,2,3,4,5,6,7,8,9},
 //						 {6,7,8,9,1,2,3,4,5},
@@ -50,69 +50,31 @@ public class Sudoku {
 		
 		
 		Sudoku sudoku = new Sudoku();
+		int[][] out = sudoku.solver(solMat, sudoku.possibilities);
+		out = sudoku.recursiveSudoku(out, sudoku.possibilities);
+		//sudoku.possGoThrough(solMat, sudoku.possibilities);
 		
 		
-		boolean change = true;
-		int counter = 0;
 		
-		while(counter <= 1) {
-		
-			
-			int[][] old = sudoku.copy2dArray(solMat);
-			int[][] current = sudoku.solver(solMat);
-			
-			change = sudoku.changes(old, current);
-			old = current;
-			
-			counter++;	
-		}
-		
-		
-		int emptyfields = 0;
-		
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
+//		ArrayList[][] test = sudoku.copy2dArrayList(sudoku.possibilities);
+//		
+//		for(int i = 0; i < 9; i++) {
+//			for(int j = 0; j < 9; j++) {
 //				System.out.print(sudoku.possibilities[i][j].toString() + " , ");
-				if(solMat[i][j] == 0) {
-					emptyfields++;
-				}
-			}
+//			}
 //			System.out.println();
-		}
+//		}
+//		
+//		System.out.println();
+//		
+//		for(int i = 0; i < 9; i++) {
+//			for(int j = 0; j < 9; j++) {
+//				System.out.print(test[i][j].toString() + " , ");
+//			}
+//			System.out.println();
+//		}
 		
-		
-		
-		if(sudoku.legal(solMat)) {
-			print2dArray(solMat);
-			return;
-		}
-		
-		System.out.println(emptyfields);
-		System.out.println();
-		
-		
-		//limit is at 21 emptyfields
-		boolean loopInv = false;
-		int[][] current = solMat;
-		
-		while(!loopInv) {
-			current = sudoku.processed(solMat);
-			loopInv = sudoku.legal(current);
-		}
-		
-		print2dArray(current);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		print2dArray(out);
 		
 		
 	}
@@ -137,39 +99,102 @@ public class Sudoku {
 		
 	}
 	
-
-	public int[][] solver (int[][] mat){
-		int[][] out = new int[9][9];
+	
+	public int[][] solver(int[][] mat, ArrayList[][] poss){
+		boolean change = true;
+		int[][] current = copy2dArray(mat);
+		ArrayList[][] newPoss = copy2dArrayList(poss);
+			
+		while(change) {
+			
+			int[][] old = copy2dArray(current);
+			
+			newPoss = adjustDefRowCol(current, newPoss);
+			newPoss = adjustDefSquare(current, newPoss);
+			
+			current = checkPoss(current, newPoss);
+			
+			change = changes(old, current);
+			
+		}
 		
-		
-		adjustDefRowCol(mat);
-		adjustDefSquare(mat);
-		
-		out = checkPoss(mat);
-		return out;
+		return current;
 	}
 	
-//	goes through the alrd filled-in numbers and calls adjustPoss whenever u is not 0, with the coordinates X, Y and u
-	public void adjustDefRowCol(int[][] mat) {
+	public int[][] recursiveSudoku(int[][] mat, ArrayList[][] poss) {
+		
+		int[][] mat1 = copy2dArray(mat);
+		
+		ArrayList[][] newPoss = copy2dArrayList(poss);
+		
+		
+		int x = -1;
+		int y = -1;
+		
+		boolean stop = false;
+		
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
-				int u = mat[i][j];
-				if(u != 0) {
-					adjustPoss(j, i, u);
+				if(mat1[i][j] == 0) {
+					x = j;
+					y = i;
+					stop = true;
+					break;
 				}
 			}
+			if(stop) {
+				break;
+			}
 		}
+		
+
+		for(int i = 0; i < poss[y][x].size(); i++) {
+			mat1[y][x] = (int) poss[y][x].get(i);
+			int[][] mat2 = solver(mat1, newPoss);
+			
+			if(legal(mat2)) {
+				return mat1;
+			} else {
+				return recursiveSudoku(mat1, newPoss);
+			}	
+		}
+		
+		return null;
 	}
-//	goes through the alrd filled-in numbers and calls adjustSquare whenever u is not 0, with the coordinates X, Y and u
-	public void adjustDefSquare(int[][] mat) {
+	
+	
+//	goes through the alrd filled-in numbers and calls adjustPoss whenever u is not 0, with the coordinates X, Y and u
+	public ArrayList<Integer>[][] adjustDefRowCol(int[][] mat, ArrayList[][] poss) {
+		
+		ArrayList[][] newPoss = copy2dArrayList(poss);
+		
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
 				int u = mat[i][j];
 				if(u != 0) {
-					adjustSquare(j, i, u);
+					newPoss = adjustPoss(j, i, u, newPoss);
 				}
 			}
 		}
+		
+		return newPoss;
+	}
+	
+//	goes through the alrd filled-in numbers and calls adjustSquare whenever u is not 0, with the coordinates X, Y and u
+	public ArrayList<Integer>[][] adjustDefSquare(int[][] mat, ArrayList[][] poss) {
+		
+		ArrayList[][] newPoss = copy2dArrayList(poss);
+		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				int u = mat[i][j];
+				if(u != 0) {
+					newPoss = adjustSquare(j, i, u, newPoss);
+				}
+			}
+		}
+		
+		return newPoss;
 	}
 	
 	public int[][] processed(int[][] mat){
@@ -185,17 +210,20 @@ public class Sudoku {
 	}
 	
 //	removes the integer u from every column and row, except from the original position
-	public void adjustPoss(int x, int y, Integer u) {
+	public ArrayList<Integer>[][] adjustPoss(int x, int y, Integer u, ArrayList[][] poss) {
+		
+		ArrayList[][] newPoss = copy2dArrayList(poss);
+		
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
 				if(i == y || j == x) {
 					if(i == y && j == x) {
-						if(this.possibilities[i][j].size() > 1) {
-							this.possibilities[i][j].removeIf(n -> (n != u));
+						if(newPoss[i][j].size() > 1) {
+							newPoss[i][j].removeIf(n -> (n != u));
 						}
 					} else {
-						if(this.possibilities[i][j].size() > 1) {
-							this.possibilities[i][j].removeIf(n -> (n == u));
+						if(newPoss[i][j].size() > 1) {
+							newPoss[i][j].removeIf(n -> (n == u));
 						}
 						
 					}
@@ -203,37 +231,40 @@ public class Sudoku {
 			}
 		}
 		
-		
-		
+		return newPoss;
 		
 	}
 	
 //	removes the integer u, in the possibilities of every position in the square, except his own
-	public void adjustSquare(int x, int y, Integer u) {
+	public ArrayList<Integer>[][] adjustSquare(int x, int y, Integer u, ArrayList[][] poss) {
 		int square = corresSquare(x, y);
 		
 		int xStart = startingCoordinates(square, "x");
 		int yStart = startingCoordinates(square, "y");
 		
+		ArrayList[][] newPoss = copy2dArrayList(poss);
+		
 		for(int i = yStart; i < yStart+3; i++) {
 			for(int j = xStart; j < xStart+3; j++) {
-					if(this.possibilities[i][j].size() > 1) {
+					if(newPoss[i][j].size() > 1) {
 						if(i == y && j == x) {
-							this.possibilities[i][j].removeIf(n -> (n != u));
+							newPoss[i][j].removeIf(n -> (n != u));
 						} else {
-							this.possibilities[i][j].removeIf(n -> (n == u));
+							newPoss[i][j].removeIf(n -> (n == u));
 						}
 					}
 			}
 		}
+		
+		return newPoss;		
 	}
 	
 //	goes through the possibility-Matrix, and wherever it is only one possibility, it fills it in the out-Matrix
-	public int[][] checkPoss(int[][] mat){
+	public int[][] checkPoss(int[][] mat, ArrayList[][] poss){
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
-				if(this.possibilities[i][j].size() == 1) {
-					mat[i][j] = (int) this.possibilities[i][j].get(0);
+				if(poss[i][j].size() == 1) {
+					mat[i][j] = (int) poss[i][j].get(0);
 				}
 			}
 		}
@@ -310,6 +341,18 @@ public class Sudoku {
 		
 		
 		return out;
+	}
+	
+	public ArrayList<Integer>[][] copy2dArrayList(ArrayList[][] poss){
+		ArrayList[][] newPoss = new ArrayList[9][9];
+		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				newPoss[i][j] = poss[i][j];
+			}
+		}
+		
+		return newPoss;
 	}
 	
 //	creates a random solution for the sudoku (the rules get tested in an other step)
